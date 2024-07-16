879403bc495aa75b14696e2e656cd8a195cbb806
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2024-07-16 16:34:42 +0900

    use pgx v2

--- src/utils.py
+++ src/utils.py
@@ -6,58 +6,6 @@ TRUE = jnp.bool_(True)
 FALSE = jnp.bool_(False)
 
 
-def auto_reset(step_fn, init_fn):
-    """Auto reset wrapper.
-
-    There are several concerns before staging this wrapper:
-
-    1. Final state (observation)
-    When auto restting happened, the termianl (or truncated) state/observation is replaced by initial state/observation,
-    This is not problematic if it's termination.
-    However, when truncation happened, value of truncated state/observation might be used by agent.
-    So we have to preserve the final state (observation) somewhere.
-    For example, in Gymnasium,
-
-    https://github.com/Farama-Foundation/Gymnasium/blob/main/gymnasium/wrappers/autoreset.py#L59
-
-    However, currently, truncation does *NOT* actually happens because
-    all of Pgx environments (games) are finite-horizon and terminates in reasonable # of steps.
-    (NOTE: Chess, Shogi, and Go have `max_termination_steps` option following AlphaZero approach)
-    So, curren implementation is enough (so far), and I feel implementing `final_state/observation` is useless and not necessary.
-
-    2. Performance:
-    Might harm the performance as it always generates new state.
-    Memory usage might be doubled. Need to check.
-    """
-
-    def wrapped_step_fn(state, action):
-        state = jax.lax.cond(
-            (state.terminated | state.truncated),
-            lambda: state.replace(  # type: ignore
-                _step_count=jnp.int32(0),
-                terminated=FALSE,
-                truncated=FALSE,
-                rewards=jnp.zeros_like(state.rewards),
-            ),
-            lambda: state,
-        )
-        state = step_fn(state, action)
-        state = jax.lax.cond(
-            (state.terminated | state.truncated),
-            # state is replaced by initial state,
-            # but preserve (terminated, truncated, reward)
-            lambda: init_fn(state._rng_key).replace(  # type: ignore
-                terminated=state.terminated,
-                truncated=state.truncated,
-                rewards=state.rewards,
-            ),
-            lambda: state,
-        )
-        return state
-
-    return wrapped_step_fn
-
-
 def single_play_step_two_policy_commpetitive(
     step_fn, actor_forward_pass, actor_params, opp_forward_pass, opp_params
 ):
